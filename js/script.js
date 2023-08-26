ec5148e1501a7c70c24e3248a1f2a57670b57353
yannvillellas <yvillellas@gmail.com>
2023-08-26 08:16:30 +0200

    project filters

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// MOBILE NAVBAR
 function showNavbar() {
     const header = document.querySelector('.header');
     const burger = document.querySelector('.burger');
@@ -15,3 +16,42 @@ function showNavbar() {
 }
 
 showNavbar();
+
+// PROJECTS
+function tabsFilter() {
+    const filters = document.querySelectorAll('.projects .projects__filter a');
+    const projects = document.querySelectorAll('.projects .card');
+
+    const resetActiveLinks = () => {
+        filters.forEach(filter => {
+            filter.classList.remove('active');
+        });
+    }
+
+    const showProjects = (elem) => {
+        projects.forEach(project => {
+            let filter = project.getAttribute('data-category');
+
+            if (elem === "all") {
+                project.parentNode.classList.remove('hide');
+                return;
+            }
+
+            filter !== elem ? project.parentNode.classList.add('hide') : project.parentNode.classList.remove('hide');
+        });
+    }
+
+    filters.forEach(elem => {
+        elem.addEventListener('click', (event) => {
+            event.preventDefault();
+            let filter = elem.getAttribute('data-filter');
+            showProjects(filter);
+            resetActiveLinks();
+            elem.classList.add('active');
+        });
+    });
+}
+
+tabsFilter();
+
+// FIXME
